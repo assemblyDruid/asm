b720e7128c85e85f486b8341c44409dc26785065
assemblyDruid <cfarvin@gmail.com>
2019-01-06 19:55:17 -0500

    restructured directory:

--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set projectName=01
-
-\masm32\bin\ml.exe /c /Zd /coff %projectName%.asm
-\masm32\bin\Link.exe /SUBSYSTEM:CONSOLE %projectName%.obj
-%projectName%.exe
-echo.
-echo [ END ]
--- build_nasm.sh
+++
@@ -1,5 +0,0 @@
-nasm -f elf32 $1.asm -o $1.o
-ld -m elf_i386 $1.o -o $1
-rm $1.o
-./$1
-echo $?
